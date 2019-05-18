79f700fd0a6d9dbbd6bdda1f8ce293a2d80ca7be
Trebor <2300936257@qq.com>
2019-05-18 13:35:20 +0800

    Some progress on type checking

--- LambdaCalculus/stlc_check.py
+++ LambdaCalculus/stlc_check.py
@@ -1,29 +1,8 @@
-def merge_dicts(*dicts):
-    if len(dicts) == 0:
-        return dict()
-    else:
-        return {**dicts[0], **merge_dicts(dicts[1:])}
-
-
-class AST:
-    def __init__(self, name, args):
-        self.name = name.replace('_', '%s')
-        self.arity = name.count('_')
-        self.args = args
-
-    def __repr__(self):
-        return self.name % self.args
-
-
-class ASTVar(AST):
-    def __init__(self, name):
-        super().__init__(name, ())
-
+from LambdaCalculus.stlc import Expression, Variable, Abstraction, Application, Constant, Type, FuncType, ConstantType
 
 
 """
 Judgments:
-G ctx
 G |- check x : A   [G is a context, A is a type]
 G |- x synth ~> A  [G is a context]
 G |- x lookup ~> A [G is a context]
@@ -41,14 +20,6 @@ def ctx2str(ctx) -> str:
 class Judgment:
     def __repr__(self):
         return "Judgment"
-
-
-class CtxJudgment(Judgment):
-    def __init__(self, ctx):
-        self.ctx = ctx
-
-    def __repr__(self):
-        return ctx2str(self.ctx) + ' ctx'
 
 
 class CheckJudgment(Judgment):
@@ -81,20 +52,11 @@ class LookupJudgment(Judgment):
         return f"{ctx2str(self.ctx)} |- {repr(self.var)} lookup ~> {repr(self.type)}"
 
 
-def pattern(pat: AST, expr: AST):
-    if isinstance(pat, ASTVar):
-        return {pat.name: expr}
-    if pat.name == expr.name:
-        return merge_dicts(*map(lambda t: pattern(*t), zip(pat.args, expr.args)))
-    raise ValueError("Pattern does not match expression.")
-
-
 """
 Typing Rules:
 ###### Context ###### (implemented as lists of tuples)
-                   G ctx
--------EMPTY   ------------EXT
- * ctx          G, x:A ctx
+
+(Context validity is automatic)
 
 
 -------------------------STOP
@@ -126,4 +88,17 @@ Typing Rules:
  G |- ^x:A.E synth ~> A->B     G |- check ^x:A.E : A->B
  """
 
-# TODO
+
+def check_judgment(j:Judgment):  # TODO: should be implemented as seperate functions?
+    if isinstance(j, LookupJudgment):
+        if j.ctx[-1][0] == j.var:
+            return None
+        else:
+            return check_judgment(LookupJudgment(j.ctx[:-1], j.var, j.type))
+    elif isinstance(j, CheckJudgment):
+        if isinstance(j.term, Variable):  # VC
+            pass
+        elif isinstance(j.term, Application):  # AC
+            pass
+        elif isinstance(j.term, Abstraction):  # LC
+            pass
