e1fe11902d7bed41f954774062cb730f337fa686
Trebor Huang <2300936257@qq.com>
2019-11-05 17:48:45 +0800

    FINISH METAVARIABLE SUBSTITUTION
    Also updated .getignore for haskell files

--- ABT.hs
+++ ABT.hs
@@ -21,15 +21,15 @@ instance Show Substitution where
 
 compose :: Substitution -> Substitution -> Substitution
 compose s           (Shift 0) = s
-compose (Dot e s)   (Shift k) = compose s (Shift (k-1))
+compose (Dot _ s)   (Shift k) = compose s (Shift (k-1))
 compose (Shift m)   (Shift n) = Shift (m+n)
 compose s           (Dot e t) = Dot (substitute e s) (compose s t)
 
 substitute :: ABT -> Substitution -> ABT
 
 substitute (Var m)  (Shift k) = Var (k+m)
-substitute (Var 0)  (Dot e s) = e
-substitute (Var k)  (Dot e s) = substitute (Var (k-1)) s
+substitute (Var 0)  (Dot e _) = e
+substitute (Var k)  (Dot _ s) = substitute (Var (k-1)) s
 substitute (Bind e) s         = Bind (substitute e (Dot (Var 0) (compose (Shift 1) s)))
 substitute (Node name abts) s = Node name (map (`substitute` s) abts)
 substitute (MetaVar n c)    s = MetaVar n (compose s c)
@@ -41,5 +41,13 @@ beta :: ABT -> ABT -> ABT
 beta (Bind e1) e2   = substitute e1 (Dot e2 (Shift 0))
 beta _         _    = error "beta is for Bind only."
 
-metaSubstitute :: ABT -> [(ABT, ABT)] -> Maybe ABT
--- TODO
+-- data ABT = Var VarName | Node NodeType [ABT] | Bind ABT | MetaVar String Substitution deriving (Eq)
+metaSubstitute :: ABT -> [(String, ABT)] -> ABT
+metaSubstitute p               []            = p
+metaSubstitute m@(MetaVar n s) ((n', e) : _) | n == n'   = substitute e s
+                                             | otherwise = m
+metaSubstitute v@(Var _)       _             = v
+metaSubstitute (Node nt abts)  msubs         = Node nt (map (`metaSubstitute` msubs) abts)
+metaSubstitute (Bind abt)      msubs         = Bind (metaSubstitute abt msubs)
+
+
